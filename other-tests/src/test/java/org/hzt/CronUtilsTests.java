725451841e3ebdaaf3cf4281d20411353a403fb3
Hans Zuidervaart <49289674+hanszt@users.noreply.github.com>
2022-12-14 14:43:56 +0100

    Update CronUtilsTests.java
    Added cron tests

--- other-tests/src/test/java/org/hzt/CronUtilsTests.java
+++ other-tests/src/test/java/org/hzt/CronUtilsTests.java
@@ -31,4 +31,91 @@ class CronUtilsTests {
 
         assertEquals("0 * * L-3 * ? *", cronAsString);
     }
+    
+    @Test
+    void testCronZonedDateTime() {
+        var cron = new Cron("*/5 * * * *");
+        var date = ZonedDateTime.parse("2000-01-01T09:00:00+01:00[Europe/Paris]");
+
+        final var next = cron.next(date);
+        final var prev = cron.prev(date);
+
+        System.out.println("Next execution: " + next);
+        System.out.println("Prev execution: " + prev);
+
+        assertAll(
+                () -> assertEquals("2000-01-01T09:05+01:00[Europe/Paris]", next.toString()),
+                () -> assertEquals("2000-01-01T08:55+01:00[Europe/Paris]", prev.toString())
+        );
+    }
+
+    @Test
+    void cronUsingFields() {
+        final var starField = new StarField();
+        final var cron = new Cron(new ValueField(FieldType.SECOND, 5), starField, starField, starField, starField, starField);
+        var date = ZonedDateTime.parse("2000-01-01T09:00:00+01:00[Europe/Paris]");
+
+        final var next = cron.next(date);
+        final var prev = cron.prev(date);
+
+        System.out.println("Next execution: " + next);
+        System.out.println("Prev execution: " + prev);
+
+        assertAll(
+                () -> assertEquals("2000-01-01T09:00:05+01:00[Europe/Paris]", next.toString()),
+                () -> assertEquals("2000-01-01T08:59:05+01:00[Europe/Paris]", prev.toString())
+        );
+    }
+
+    @ParameterizedTest(name = "alias: `{0}` should return the same schedule as cron: `{1}`")
+    @CsvSource(value = {
+            "@yearly, 0 0 1 1 *",
+            "@annually, 0 0 1 1 *",
+            "@monthly, 0 0 1 * *",
+            "@weekly, 0 0 * * 0",
+            "@daily, 0 0 * * *",
+            "@midnight, 0 0 * * *",
+            "@hourly, 0 * * * *",
+    })
+    void testAliases(String alias, String cron) {
+        Cron cronAlias = new Cron(alias);
+        Cron cronCron = new Cron(cron);
+
+        ZonedDateTime dateByAlias = Support.brt("2016-10-15 13:14:00");
+        ZonedDateTime dateByCron = Support.brt("2016-10-15 13:14:00");
+
+        for (int i = 0; i < 1000; i++) {
+            dateByAlias = cronAlias.next(dateByAlias);
+            dateByCron = cronCron.next(dateByCron);
+
+            ZonedDateTime finalNextDateByCron = dateByCron;
+            ZonedDateTime finalNextDateByAlias = dateByAlias;
+
+            assertAll(
+                    () -> assertTrue(cronAlias.matches(finalNextDateByCron)),
+                    () -> assertTrue(cronCron.matches(finalNextDateByAlias)),
+                    () -> assertEquals(finalNextDateByAlias, finalNextDateByCron)
+            );
+        }
+    }
+
+    private static class Support {
+
+        private static final DateTimeFormatter FORMATTER =
+                new DateTimeFormatterBuilder()
+                        .appendPattern("yyyy-MM-dd HH:mm:ss") // .parseLenient()
+                        .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+                        .toFormatter();
+
+        public static final DateTimeFormatter BRT = FORMATTER.withZone(ZoneId.of("America/Sao_Paulo"));
+        public static final DateTimeFormatter UTC = FORMATTER.withZone(ZoneId.of("UTC"));
+
+        public static ZonedDateTime brt(String s) {
+            return ZonedDateTime.parse(s, BRT);
+        }
+
+        public static ZonedDateTime utc(String s) {
+            return ZonedDateTime.parse(s, UTC);
+        }
+    }
 }
