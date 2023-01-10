7d1839e9762f7aa6ab0fe3247e4e98eb2022546d
HZUIDERV <hans.zuidervaart@sogeti.com>
2023-01-10 17:26:45 +0100

    - Added tomcat instance and runconfig - Updated dependencies

--- clean-all.cmd
+++ clean-all.cmd
@@ -1,3 +1,7 @@
 call mvn clean
 cd latest-jdk-practice-module
 call mvn clean
+cd ..\rust-from-java\mylib
+call cargo clean
+cd ..\optimize
+call cargo clean
--- other-tests/src/test/java/org/hzt/RecursiveSamplesKtTest.kt
+++ other-tests/src/test/java/org/hzt/RecursiveSamplesKtTest.kt
@@ -1,15 +1,13 @@
 package org.hzt
 
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 internal class RecursiveSamplesKtTest {
 
     @Test
     fun `test fib with cash`() {
         val n = 5_000
-        val fib = fib(n)
-        val expected = RecursiveSamples.fibWithCash(n)
-        assertEquals(expected, fib)
+        fib(n) shouldBe RecursiveSamples.fibWithCash(n)
     }
 }
