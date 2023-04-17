c9db77ceeb6cf9f3ef9deeb794f50b124842f943
HZUIDERV <hans.zuidervaart@sogeti.com>
2023-04-17 11:07:25 +0200

    - Updated RungeKutta integrator

--- other-tests/src/main/java/org/hzt/RungeKutta.java
+++ other-tests/src/main/java/org/hzt/RungeKutta.java
@@ -1,6 +1,6 @@
 package org.hzt;
 
-import java.util.function.DoubleBinaryOperator;
+import java.util.function.UnaryOperator;
 
 /**
  * @see <a href="https://rosettacode.org/wiki/Runge-Kutta_method">Runge kutta method</a>
@@ -10,16 +10,26 @@ public final class RungeKutta {
     private RungeKutta() {
     }
 
-    static void rungeKutta4(DoubleBinaryOperator yp_func, double[] t, double[] y, double dt) {
+    public static double[] rungeKutta4ChatGpt(double[] vector, double h, UnaryOperator<double[]> solveOdes) {
+        double[] k1 = solveOdes.apply(vector);
+        double[] k2 = solveOdes.apply(addElementWise(vector, k1, h / 2));
+        double[] k3 = solveOdes.apply(addElementWise(vector, k2, h / 2));
+        double[] k4 = solveOdes.apply(addElementWise(vector, k3, h));
 
-        for (int n = 0; n < t.length - 1; n++) {
-            final double dy1 = dt * yp_func.applyAsDouble(t[n], y[n]);
-            final double dy2 = dt * yp_func.applyAsDouble(t[n] + dt / 2.0, y[n] + dy1 / 2.0);
-            final double dy3 = dt * yp_func.applyAsDouble(t[n] + dt / 2.0, y[n] + dy2 / 2.0);
-            final double dy4 = dt * yp_func.applyAsDouble(t[n] + dt, y[n] + dy3);
-            t[n + 1] = t[n] + dt;
-            y[n + 1] = y[n] + (dy1 + 2.0 * (dy2 + dy3) + dy4) / 6.0;
+        final var resultVector = new double[vector.length];
+        // Update the variables
+        for (int i = 0; i < vector.length; i++) {
+            resultVector[i] = vector[i] + (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) * h / 6;
         }
+        return resultVector;
+    }
 
+    // Helper method to add two arrays element-wise
+    private static double[] addElementWise(double[] x, double[] k, double h) {
+        double[] result = new double[x.length];
+        for (int i = 0; i < x.length; i++) {
+            result[i] = x[i] + k[i] * h;
+        }
+        return result;
     }
 }
